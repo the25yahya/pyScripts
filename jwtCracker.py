229810576c804833984e30fd5a6b75c8625eebb0
the25yahya <114318659+the25yahya@users.noreply.github.com>
2024-05-30 06:36:01 +0200

    minor change

--- jwtCracker.py
+++ jwtCracker.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 
 hash = input("\033[1m Enter jwt token hash to crack : \033[0m")
-file_path = Path('C:/Users/net/Desktop/SecLists/Passwords/xato-net-10-million-passwords-1000000.txt')
+file_path = Path('C:/Users/net/Desktop/SecLists/google-10000-english/google-10000-english-usa.txt')
 
 flag = False
 count = 0
@@ -31,6 +31,7 @@ else :
             if newHash == hash:
                print("\033[91m\033[1m Secret key found  \033[0m : ", password)
                flag = True
+               break
         if not flag:
               print(f"\033[91m\033[1m key wasnt found, try again with another wordlist, tried {count} combinations \033[0m")
     except Exception as e:
@@ -42,8 +43,3 @@ hash = input("\033[1m Enter jwt token hash to crack : \033[0m")
 
 
     
-    
-#jwt.decode(encoded_jwt, "secret", algorithms=["HS256"])
-#{'some': 'payload'}
-
-#eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJXZWJHb2F0IFRva2VuIEJ1aWxkZXIiLCJhdWQiOiJ3ZWJnb2F0Lm9yZyIsImlhdCI6MTcxNjk2NTUxNywiZXhwIjoxNzE2OTY1NTc3LCJzdWIiOiJ0b21Ad2ViZ29hdC5vcmciLCJ1c2VybmFtZSI6IlRvbSIsIkVtYWlsIjoidG9tQHdlYmdvYXQub3JnIiwiUm9sZSI6WyJNYW5hZ2VyIiwiUHJvamVjdCBBZG1pbmlzdHJhdG9yIl19.lYb8COgG-lGh966w7xovaRfsE6thcupGfxzQCDCqqdo
